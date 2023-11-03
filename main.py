4223f84a50b2f2074ebd7875ac3fd1f1c0ee69c8
TiD <nsofi2009@gmail.com>
2023-11-03 20:42:14 +0200

    0.5

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+class Human:
+    def __init__(self, name="Human", job=None, home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.home = home
+        self.car = car
