b54fd7c6f10e390532e9192a1f81d78f1c1497cc
TiD <nsofi2009@gmail.com>
2023-11-03 21:28:54 +0200

    0.5

--- main.py
+++ main.py
@@ -9,3 +9,74 @@ class Human:
         self.job = job
         self.home = home
         self.car = car
+
+    def get_home(self):
+        self.home = House()
+
+    def get_car(self):
+        self.car = Auto(brands_of_car)
+
+    def get_job(self):
+        if self.car.drive():
+            pass
+        else:
+            self.to_rapair()
+            return
+        self.job = Job(job_list)
+
+    # def eat(self):
+    #     if self.home.foot <= 0:
+    #         self.shoping("food")
+
+    def clean_home(self):
+        self.mess = 0
+        self.gladness -= 50
+
+    def chill(self):
+        self.mess += 5
+        self.gladness += 10
+
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+
+class Auto:
+    def __init__(self, brand_list):
+        self.brand = random.choice(list(brand_list))
+        self.fuel = brand_list[self.brand]["fuel"]
+        self.strength = brand_list[self.brand]["strength"]
+        self.consumption = brand_list[self.brand]["consumption"]
+
+    def drive(self):
+        if self.strength > 0 and self.fuel >= self.consumption:
+            self.fuel -= self.consumption
+            self.strength -= 1
+            return True
+        else:
+            print("The car cannot move!")
+            return False
+
+    def to_repair(self):
+        self.strength += 100
+        self.money -= 50
+
+class Job:
+    def __init__(self):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]['salary']
+        self.gladnesss_less = job_list[self.job]['gladness_less']
+
+job_list = {
+    "Java developer": {"salary": 50, "gladness_less": 10},
+    "Python developer": {"salary": 40, "gladness_less": 3},
+    "c++ developer": {"salary": 60, "gladness_less": 25},
+    "Rust developer": {"salary": 70, "gladness_less": 15}
+}
+
+brands_of_car = {
+    "BMW": {"fuel": 100, "strength": 100, "consumpition": 6},
+    "Lada": {"fuel": 50, "strength": 40, "consumpition": 10},
+    "Volvo": {"fuel": 80, "strength": 150, "consumpition": 8},
+    "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14}
+}
